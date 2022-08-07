f6be5d6792b47df815c9d9b1fd7a722bf74980f6
FandyRdh <framadhana012@gmail.com>
2022-08-08 06:57:26 +0700

    laravel-wpu latihan_3

--- laravel/coba-laravel/routes/web.php
+++ laravel/coba-laravel/routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        'nama' => 'FandyRdh',
+        'email' => 'FandyRdh@gmail.com',
+        'image' => 'contohImg.jpg',
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
